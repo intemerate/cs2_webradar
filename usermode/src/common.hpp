49a6c1cb1190d2f12dccf60039761d97d99a60f3
$torm <stormzzyyne@gmail.com>
2026-02-16 11:40:23 +0200

    Update CS2_WEBRADAR_VERSION and game signatures

--- usermode/src/common.hpp
+++ usermode/src/common.hpp
@@ -2,6 +2,7 @@
 
 /* current build of cs2_webradar */
 #define CS2_WEBRADAR_VERSION "v1.2.8"
+#define CS2_WEBRADAR_VERSION "v1.2.9"
 
 /* game modules */
 #define CLIENT_DLL "client.dll"
@@ -10,6 +11,7 @@
 
 /* game signatures */
 #define GET_SCHEMA_SYSTEM "48 89 05 ? ? ? ? 4c 8d 0d ? ? ? ? 0f b6 45 ? 4c 8d 45 ? 33 f6"
+#define GET_SCHEMA_SYSTEM "48 89 05 ? ? ? ? 4c 8d 0d ? ? ? ? 33 c0"
 #define GET_ENTITY_LIST "48 8b 0d ? ? ? ? 48 89 7c 24 ? 8b fa c1 eb"
 #define GET_GLOBAL_VARS "48 89 15 ? ? ? ? 48 89 42"
 #define GET_LOCAL_PLAYER_CONTROLLER "4c 8d 05 ? ? ? ? 33 d2 4d 8b 04 c0"
@@ -26,4 +28,3 @@
         const auto filename = std::filesystem::path(__FILE__).filename().string(); \
         printf(" [error] [%s:%d] " str "\n", filename.c_str(), __LINE__, __VA_ARGS__); \
         std::this_thread::sleep_for(std::chrono::seconds(5)); \
-    }
